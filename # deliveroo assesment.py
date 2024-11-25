da15bbb795ed3f28d1e7b29e0d2225b7b1060fc7
RAYANE8LOUNIS <rayanelounis@icloud.com>
2024-11-25 21:35:26 +0000

    take order

--- # deliveroo assesment.py
+++ # deliveroo assesment.py
@@ -6,3 +6,13 @@ import os
 # from Pillow import image, ImageTk
 # File to store orders
 ORDERS_FILE = "rayane.order"
+
+def TAke_orders():
+    """Load orders from a file, and creating it if it doesn't exist for more help."""
+    try:
+        if not os.path.exists(ORDERS_FILE):
+            with open(ORDERS_FILE, 'w') as file:
+                json.dump([], file)  #this should create an empty list if file doesn't exist for more efficasity 
+        with open(ORDERS_FILE, 'r') as file:
+            return json.load(file)
+   
