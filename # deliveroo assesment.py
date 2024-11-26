a2c84f67d831e6a17fcfb1e154fe3b5e618e306a
RAYANE8LOUNIS <rayanelounis@icloud.com>
2024-11-26 00:56:18 +0000

    finish the display window

--- # deliveroo assesment.py
+++ # deliveroo assesment.py
@@ -41,4 +41,7 @@ STYLE = {
 def center_window(window, width=600, height=400):
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
+    window.geometry(f"{width}x{height}+{x}+{y}")
     
